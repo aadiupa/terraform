aa7a29f9612ee3fd2cd859336fe56a1c24686b1c
Aditya <aditya.upadhyay@buyerassist.io>
2022-03-10 15:55:55 +0530

    changes in methodology

--- terrafortesting/ec2_instance.tf
+++ terrafortesting/ec2_instance.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "firstecd2" {
-    ami = "ami-0c1a7f89451184c8b"
-    instance_type = "t2.micro"
+  ami           = "ami-0c1a7f89451184c8b"
+  instance_type = "t2.micro"
 }
 
